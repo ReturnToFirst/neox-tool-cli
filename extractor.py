d2491ce4ab513b700550e095618d3201f7d5874b
MarcosVLl2 <marcosmola2@gmail.com>
2024-08-14 21:49:45 +0100

    Version 1.1
    Fixed nxs3 unpacking capability, added .bip extraction and fixed some other file detection algorithms, added new "--nxs3" command

--- extractor.py
+++ extractor.py
@@ -1,7 +1,8 @@
-import shutil
+import shutil, mmh3
 import os, struct, zlib, tempfile, argparse, zstandard, lz4.block, zipfile
+import rotor, time
 from key import Keys
-import rotor
+
 
 def readuint64(f):
     return struct.unpack('Q', f.read(8))[0]
@@ -28,21 +29,24 @@ def print_data(verblevel, text, data, typeofdata, pointer=0):
         case 5:
             print("{:10} {} {}   DATA TYPE:{}".format(pointer, text, data, typeofdata))
 
+def get_compression(data):
+    if len(data) == 0:
+        return 'none'
+    elif data[:2] == bytes([0x1D, 0x04]) or data[:2] == bytes([0x15, 0x23]):
+        return 'rot'
+    elif data[0:8] == b"NXS3\x03\x00\x00\x01":
+        return 'nxs3'
+    elif data[:4] == bytes([0x50, 0x4B, 0x03, 0x04]) or data[:4] == bytes([0x50, 0x4B, 0x05, 0x06]):
+        return 'zip'
+    return 'none'
+
 def get_ext(data):
     if len(data) == 0:
         return 'none'
-    if data[:12] == b'CocosStudio-UI':
+    elif data[:12] == b'CocosStudio-UI':
         return 'coc'
-    elif data[:4] == bytes([0x28, 0xB5, 0x2F, 0xFD]):
-        return 'zst'
-    elif data[:4] == bytes([0x50, 0x4B, 0x03, 0x04]) or data[:4] == bytes([0x50, 0x4B, 0x05, 0x06]):
-        return 'zip'
     elif data[:8] == b'SKELETON':
         return 'skeleton'
-    elif data[:1] == b'%':
-        return 'tpl'
-    elif data[:1] == b'{':
-        return 'json'
     elif data[:3] == b'hit':
         return 'hit'
     elif data[:3] == b'PKM':
@@ -61,8 +65,6 @@ def get_ext(data):
         return 'ktx'
     elif data[1:4] == b'PNG':
         return 'png'
-    elif data[:2] == bytes([0x28, 0xB5]) or data[:2] == bytes([0x1D, 0x04]) or data[:2] == bytes([0x15, 0x23]):
-        return 'rot'
     elif data[:4] == bytes([0x34, 0x80, 0xC8, 0xBB]):
         return 'mesh'
     elif data[:4] == bytes([0x14, 0x00, 0x00, 0x00]):
@@ -77,8 +79,6 @@ def get_ext(data):
         return 'mdmp'
     elif data[:4] == b'RGIS':
         return 'gis'
-    elif data[7:15] == bytes([0x4E, 0x58, 0x53, 0x33, 0x03, 0x00, 0x00, 0x01]):
-        return 'nxs3'
     elif data[:4] == b'NTRK':
         return 'ntrk'
     elif data[:4] == b'RIFF':
@@ -89,7 +89,11 @@ def get_ext(data):
         return 'pem'
     elif data[:1] == b'<':
         return 'xml'
-    elif data[:4] == bytes([0xE3, 0x00, 0x00, 0x00]) or data[:4] == bytes([0x63, 0x00, 0x00, 0x00]):
+    elif data[:1] == b'%':
+        return 'tpl'
+    elif data[:1] == b'{':
+        return 'json'
+    elif data[:4] == bytes([0xE3, 0x00, 0x00, 0x00]) or data[:4] == bytes([0x63, 0x00, 0x00, 0x00]) or data[2:4] == bytes([0x0D, 0x0A]):
         return 'pyc'
     elif len(data) < 1000000:
         if b'package google.protobuf' in data:
@@ -109,7 +113,7 @@ def get_ext(data):
         if b'Javascript' in data:
             return 'js'
         if b'biped' in data or b'bip001' in data or b'bone' in data or b'bone001' in data or b'bip01' in data:
-            return 'model'
+            return 'bip'
         if b'div.document' in data:
             return 'css'
     return 'dat'
@@ -166,20 +170,28 @@ def unpack(args, statusBar=None):
             print_data(args.info, "ENCRYPTMODE:", encryption_mode, "NXPK_DATA", f.tell())
             hash_mode = readuint32(f)
             print_data(args.info, "HASHMODE:", hash_mode, "NXPK_DATA", f.tell())
-            mode = 1 if var1 and hash_mode else 0
-            info_size = 0x28 if mode else 0x1c
+            #mode = 1 if var1 and hash_mode else 0
+            #info_size = 0x28 if mode else 0x1c
             index_offset = readuint32(f)
             print_data(args.info, "INDEXOFFSET:", index_offset, "NXPK_DATA", f.tell())
             print("")
 
             index_table = []
             nxfn_files = []
+            hash_table = {}
+            test = False
 
             if encryption_mode == 256 and args.nxfn_file:
                 with open(folder_path+"/NXFN_result.txt", "w") as nxfn:
                     f.seek(index_offset + (files * 28) + 16)
                     nxfn_files = [x for x in (f.read()).split(b'\x00') if x != b'']
                     for nxfnline in nxfn_files:
+                        complete_hash = int(mmh3.hash(nxfnline, signed=False)) # 0x66666666
+                        if not test:
+                            test = True
+                            print(args)
+                            print(complete_hash)
+                        hash_table.update({nxfnline:complete_hash})
                         nxfn.write(nxfnline.decode() + "\n")
             elif encryption_mode == 256:
                 f.seek(index_offset + (files * 28) + 16)
@@ -194,7 +206,7 @@ def unpack(args, statusBar=None):
                     data = keys.decrypt(data)
                 tmp.write(data)
                 tmp.seek(0)
-                for x in range(files):
+                if args.do_one:
                     file_sign = [readuint32(tmp), tmp.tell()]
                     file_offset = readuint32(tmp)
                     file_length = readuint32(tmp)
@@ -203,7 +215,7 @@ def unpack(args, statusBar=None):
                     crc = readuint32(tmp)                 #decompressed crc
                     zip_flag = readuint16(tmp)
                     file_flag = readuint16(tmp)
-                    file_structure = nxfn_files[x] if nxfn_files else None
+                    file_structure = nxfn_files[0] if nxfn_files else None
                     index_table.append((
                         file_sign,
                         file_offset, 
@@ -215,22 +227,52 @@ def unpack(args, statusBar=None):
                         zip_flag,
                         file_flag,
                         ))
+                else:
+                    for x in range(files):
+                        file_sign = [readuint32(tmp), tmp.tell()]
+                        file_offset = readuint32(tmp)
+                        file_length = readuint32(tmp)
+                        file_original_length = readuint32(tmp)
+                        zcrc = readuint32(tmp)                #compressed crc
+                        crc = readuint32(tmp)                 #decompressed crc
+                        zip_flag = readuint16(tmp)
+                        file_flag = readuint16(tmp)
+                        file_structure = nxfn_files[x] if nxfn_files else None
+                        index_table.append((
+                            file_sign,
+                            file_offset, 
+                            file_length,
+                            file_original_length,
+                            zcrc,
+                            crc,
+                            file_structure,
+                            zip_flag,
+                            file_flag,
+                            ))
             step = files // 50 + 1
             
             for i, item in enumerate(index_table):
+                data2 = None
                 if (i % step == 0 or i + 1 == files and args.info < 2) or args.info > 2:
                     print('FILE: {}/{}'.format(i + 1, files))
                 file_sign, file_offset, file_length, file_original_length, zcrc, crc, file_structure, zflag, file_flag = item
                 print_data(args.info, "FILESIGN:", file_sign[0], "VERBOSE_FILE", file_sign[1])
                 print_data(args.info, "FILEOFFSET:", file_offset, "FILE", file_sign[1] + 4)
                 print_data(args.info, "FILELENGTH:", file_length, "VERBOSE_FILE", file_sign[1] + 8)
-                print_data(args.info, "FILEORIGLENGTH:", file_original_length, "FILE", file_sign[1] + 12)
+                print_data(args.info, "FILEORIGLENGTH:", file_original_length, "VERBOSE_FILE", file_sign[1] + 12)
                 print_data(args.info, "ZIPCRCFLAG:", zcrc, "VERBOSE_FILE", file_sign[1] + 16)
                 print_data(args.info, "CRCFLAG:", crc, "VERBOSE_FILE", file_sign[1] + 20)
                 print_data(args.info, "ZFLAG:", zflag, "VERBOSE_FILE", file_sign[1] + 22)
                 print_data(args.info, "FILEFLAG:", file_flag, "VERBOSE_FILE", file_sign[1] + 24)
                 f.seek(file_offset)
                 data = f.read(file_length)
+
+                def check_file_structure(ext):
+                    if file_structure and not args.no_nxfn:
+                        file_path = folder_path + "/" + file_structure.decode().replace("\\", "/")
+                    else:
+                        file_path = folder_path + '/{:08}.{}'.format(i, ext)
+                    return file_path
 
                 if pkg_type:
                     data = keys.decrypt(data)
@@ -256,7 +298,7 @@ def unpack(args, statusBar=None):
                     offset = 0
                     length = 0                    
 
-                    if file_length < 0x81:
+                    if file_length < 0x80:
                         length = file_length
                     else:
                         offset = (v3 >> 1) % (file_length - 0x80)
@@ -267,63 +309,68 @@ def unpack(args, statusBar=None):
 
                     for xx in range(offset, min(offset + length, file_original_length)):
                         data[xx] ^= key
-                        key = (key + 1) & 0xff
-                    data = bytes(data)
-                    
+                        key = (key + 1) & 0xff                    
 
-                ext = get_ext(data)
+                compression = get_compression(data)
 
-                if ext == "rot":
+                if compression == "rot":
                     rotor = init_rotor()
                     data = rotor.decrypt(data)
                     data = zlib.decompress(data)
                     data = _reverse_string(data)
 
-                if ext == "nxs3":
-                    with open("file.tmp") as wr:
+
+                if zflag == 1 and compression != 'rot':
+                    data = zlib.decompress(data, bufsize=file_original_length)
+                elif zflag == 2 and compression != 'rot':
+                    data = lz4.block.decompress(data, uncompressed_size=file_original_length)
+                elif zflag == 3 and compression != 'rot':
+                    data = zstandard.ZstdDecompressor().decompress(data)
+
+                file_path = check_file_structure(compression)
+
+                compression = get_compression(data)
+                print_data(args.info, "COMPRESSION:", compression.upper(), "FILE", file_offset)
+
+                if compression == "nxs3":
+                    with open("file.tmp", "wb") as wr:
                         wr.write(data)
-                    import subprocess
-                    os.system('./de_nxs3 file.tmp done.tmp')
+                    os.system('./denxs3')
+                    #time.sleep(0.0001) #if youre getting the error "done.tmp does not exist", try incrementing the sleep time
+                    data2 = data
                     with open("done.tmp", "rb") as rd:
                         data = rd.read()
                     os.remove("file.tmp")
                     os.remove("done.tmp")
 
-                if zflag == 1 and ext != 'rot':
-                    data = zlib.decompress(data)
-                elif zflag == 2 and ext != 'rot':
-                    data = lz4.block.decompress(data, uncompressed_size=2147483647)
-
-                ext = get_ext(data)
-
-                if file_structure and not args.no_nxfn:
-                    print_data(args.info, "FILENAME:", file_structure, "FILE", file_offset)
-                    file_path = folder_path + "/" + file_structure.decode().replace("\\", "/")
-                else:
-                    file_path = folder_path + '/{:08}.{}'.format(i, ext)
-                os.makedirs(os.path.dirname(file_path), exist_ok=True)
-                if ext == 'zst':
-                    if not args.delete_compressed:
-                        with open(file_path + ".zst", 'wb') as dat:
-                            dat.write(data)
-                    dctx = zstandard.ZstdDecompressor()
-                    dctx.decompress(data)
-                    with open(file_path, 'wb') as dat:
-                        dat.write(data)
-                elif ext == 'zip':
-                    with open(file_path, 'wb') as dat:
+                if compression == 'zip':
+                    print_data(args.info, "FILENAME:", file_path, "FILE", file_offset)
+                    with open(file_path + ".zip", 'wb') as dat:
                         dat.write(data) 
-                    with zipfile.ZipFile(file_path, 'r') as zip:
-                        zip.extractall(file_path.replace(".zip", ""))
+                    try:
+                        get_ext(data)
+                        with zipfile.ZipFile(file_path + ".zip", 'r') as zip:
+                            zip.extractall(file_path.replace(".zip", ""))
+                    except Exception as e:
+                        print("ZIP ERROR")
                     if args.delete_compressed:
                         os.remove(file_path)
-                else:
-                    with open(file_path, 'wb') as dat:
-                        dat.write(data)  
+
+                
+                ext = get_ext(data)
+                file_path = check_file_structure(ext)
+                print_data(args.info, "FILENAME:", file_path, "FILE", file_offset)
+                with open(file_path, 'wb') as dat:
+                    dat.write(data)
+                if args.nxs3 and data2 != None:
+                    with open(file_path[:-3] + "nxs3", "wb") as dat2:
+                        dat2.write(data2)
             print("FINISHED!")
 
+
 def get_parser():
-    parser = argparse.ArgumentParser(description='NPK/EXPK Extractor', add_help=False)
+    parser = argparse.ArgumentParser(description='NXPK/EXPK Extractor', add_help=False)
+    parser.add_argument('-v','--version', action='version', version='NXPK/EXPK Extractor  ---  Version: 1.1 (added .bip and fixed .nxs3 extraction)')
     parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Show this help message and exit')
     parser.add_argument('-p','--path', help="Specify the path of the file or directory, if not specified will do all the files in the current directory",type=str)
     parser.add_argument('-d', "--delete-compressed", action="store_true",help="Delete compressed files (such as ZStandard or ZIP files) after decompression")
@@ -331,7 +378,8 @@ def get_parser():
     parser.add_argument('--nxfn-file', action="store_true",help="Writes a text file with the NXFN dump output (if applicable)")
     parser.add_argument('--no-nxfn',action="store_false",help="Disables NXFN file structure")
     parser.add_argument('--do-one', action='store_true', help='Only do the first file (TESTING PURPOSES)')
-
+    parser.add_argument('--nxs3', action='store_true', help="Keep NXS3 files if there's any")
+    #nxs_unpack()
     opt = parser.parse_args()
     return opt
 
