0cf906fb99bef970526b986a184e55bcd553e1fb
MarcosVLl2 <marcosmola2@gmail.com>
2024-09-12 00:45:25 +0100

    Version 1.7
    # Version 1.7 for the NeoX Tool (File detections galore!)
    
    Changelog:
    * Implemented detection for types of file:
    > NeoXML: .uiprefab   .decal   .lod   .pse   .nxcompute   .nxshader   .scn   .blt   .psemusic   .blendspace1d   .animgraph   .blendspace   .nxmeta   .scnex   .localweather   .gimext   .haptic   .localfogparams   .waterfalls   .xml.rels   .mirrortable   .clt   .plist   .render   .skeletonrig   .atlas   .fnt   .cache   .info   .list   .map   .md5   .sfx   .hdr   .decal
    > 
    > Audio: .wem
    > 
    > Binaries: .foliage   .clothasset   .trk (NTRK)   .binary
    * Optimized the search code (by making the most common be scanned for first)
    
    # 1.7版的NeoX工具 (文件检测大量!)
    
    更新日志: 
    * 实现对新类型文件的检测:
    > NeoXML: .uiprefab   .decal   .lod   .pse   .nxcompute   .nxshader   .scn   .blt   .psemusic   .blendspace1d   .animgraph   .blendspace   .nxmeta   .scnex   .localweather   .gimext   .haptic   .localfogparams   .waterfalls   .xml.rels   .mirrortable   .clt   .plist   .render   .skeletonrig   .atlas   .fnt   .cache   .info   .list   .map   .md5   .sfx   .hdr   .decal
    > 
    > 音频文件: .wem
    > 
    > 二进制文件: .foliage   .clothasset   .trk (NTRK)   .binary
    * 优化了搜索代码（通过使最常见的首先扫描）

--- detection.py
+++ detection.py
@@ -12,20 +12,36 @@ def get_compression(data):
 def get_ext(data):
     if len(data) == 0:
         return 'empty'
+    elif data[:3] == b'PVR':
+        return 'pvr'
+    elif data[:4] == bytes([0x34, 0x80, 0xC8, 0xBB]):
+        return 'mesh'
+    elif data[:4] == b'RIFF':
+        return 'wem'
+    elif data[:8] == b'RAWANIMA':
+        return 'rawanimation'
+    elif data[:8] == b'NEOXBIN1':
+        return 'uiprefab'
+    elif data[:8] == b'SKELETON':
+        return 'skeleton'
+    elif data[1:8] == bytes([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]):
+        return 'blasttool'
+    elif data[:6] == bytes([0x01, 0x00, 0x05, 0x00, 0x00, 0x00]):
+        return 'foliage'
+    elif data[:8] == b'NEOXMESH':
+        return 'uimesh'
+    elif data[:33] == b'NVidia(r) GameWorks Blast(tm) v.1':
+        return 'blast'
     elif data[:4] == bytes([0xE3, 0x00, 0x00, 0x00]) or data[:4] == bytes([0x63, 0x00, 0x00, 0x00]) or data[2:4] == bytes([0x0D, 0x0A]):
         return 'pyc'
     elif data[:12] == b'CocosStudio-UI':
         return 'coc'
-    elif data[:8] == b'SKELETON':
-        return 'skeleton'
     elif data[:4] == bytes([0x13, 0xAB, 0xA1, 0x5C]):
         return 'astc'
     elif data[:3] == b'hit':
         return 'hit'
     elif data[:3] == b'PKM':
         return 'pkm'
-    elif data[:3] == b'PVR':
-        return 'pvr'
     elif data[:3] == b'DDS':
         return 'dds'
     elif data[-18:-2] == b'TRUEVISION-XFILE' or data[:3] == bytes([0x00, 0x00, 0x02]) or data[:3] == bytes([0x0D, 0x00, 0x02]):
@@ -36,18 +52,12 @@ def get_ext(data):
         return 'pyi'
     elif data[1:4] == b'KTX':
         return 'ktx'
+    elif data[:9] == b'blastmesh':
+        return 'blastmesh'
+    elif data[:10] == b'clothasset':
+        return 'clothasset'
     elif data[1:4] == b'PNG':
         return 'png'
-    elif data[:4] == bytes([0x34, 0x80, 0xC8, 0xBB]):
-        return 'mesh'
-    elif data[:4] == bytes([0x14, 0x00, 0x00, 0x00]):
-        return 'type1'
-    elif data[:4] == bytes([0x04, 0x00, 0x00, 0x00]):
-        return 'type2'
-    elif data[:4] == bytes([0x00, 0x01, 0x00, 0x00]):
-        return 'type3'
-    elif data[1:8] == bytes([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]):
-        return 'blasttool'
     elif data[:4] == b'VANT':
         return 'vant'
     elif data[:4] == b'MDMP':
@@ -55,9 +65,7 @@ def get_ext(data):
     elif data[:4] == b'RGIS':
         return 'gis'
     elif data[:4] == b'NTRK':
-        return 'ntrk'
-    elif data[:4] == b'RIFF':
-        return 'riff'
+        return 'trk'
     elif data[:4] == bytes([0xFF,0xD8,0xFF,0xE1]):
         return 'jpg'
     elif data[:4] == b'BKHD':
@@ -66,54 +74,120 @@ def get_ext(data):
         return 'pem'
     elif data[:1] == b'%':
         return 'tpl'
-    elif data[:1] == b'{':
-        return 'json'
     elif data[:4] == b'TZif':
         return 'tzif'
     elif data[6:10] == b'JFIF':
         return 'jfif'
     elif data[4:8] == b'ftyp':
         return 'mp4'
-    elif data[:33] == b'NVidia(r) GameWorks Blast(tm) v.1':
-        return 'blast'
-    elif data[:8] == b'RAWANIMA':
-        return 'rawanimation'
-    elif data[:9] == b'blastmesh':
-        return 'blastmesh'
     elif len(data) < 100000000:
         #NeoXML file detection
+        if b'<Material' in data:
+            return 'mtl'
+        if b'<MaterialGroup' in data:
+            return 'mtg'
+        if b'<MetaInfo' in data:
+            return 'pvr.meta'
+        if b'ViewProjection' in data:
+            return 'binary'
+        if b'<Section' in data:
+            return 'sec'
+        if b'GeoBatchHint="1"' in data:
+            return 'gim'
+        if b'<Track' in data:
+            return 'trackgroup'
+        if b'<Instances' in data:
+            return 'decal'
+        if b'<Physics' in data:
+            return 'col'
+        if b'<LODPolicy' in data or b'<LODProfile' in data:
+            return 'lod'
         if b'Type="Animation"' in data:
             return 'animation'
+        if b'DisableBakeLightProbe=' in data:
+            return 'prefab'
+        if b'<Scene' in data:
+            return 'scn'
+        if b'"ParticleSystemTemplate"' in data:
+            return 'pse'
+        if b'<MainBody' in data:
+            return 'nxcompute'
+        if b'?xml' in data:
+            return 'xml'
+        if b'<MapSkeletonToMeshBone' in data:
+            return 'skeletonextra'
+        if b'<ShadingModel' in data:
+            return 'nxshader'
+        if b'<BlastDynamic' in data:
+            return 'blt'
+        if b'"ParticleAudio"' in data:
+            return 'psemusic'
+        if b'<BlendSpace' in data and b'is2D="false"' in data:
+            return 'blendspace1d'
         if b'<AnimationConfig' in data:
             return 'animconfig'
         if b'<AnimationGraph' in data:
             return 'animgraph'
-        if b'<Physics' in data:
-            return 'col'
-        if b'<EnvParticle' in data:
-            return 'envp'
-        if b'<MaterialGroup' in data:
-            return 'mtg'
-        if b'<Material' in data:
-            return 'mtl'
-        if b'<Chain' in data:  #needs more testing
-            return 'physicalbone'
-        if b'<PostProcess' in data:
-            return 'postprocess'
-        if b'DisableBakeLightProbe=' in data:  #needs more testing
-            return 'prefab'
-        if b'<FxGroup' in data:
-            return 'sfx'
-        if b'<MapSkeletonToMeshBone' in data:
-            return 'skeletonextra'
-        if b'<Macros' in data:
-            return 'xml.template'
         if b'<Head Type="Timeline"' in data:
             return 'timeline'
-        if b'<Track' in data:
-            return 'trackgroup'
-        if b'<MetaInfo' in data:
-            return 'pvr.meta'
+        if b'<Chain' in data:
+            return 'physicalbone'
+        if b'<BlendSpace' in data and b'is2D="true"' in data:
+            return 'blendspace'
+        if b'<PostProcess' in data:
+            return 'postprocess'
+        if b'"mesh_import_options":{' in data:
+            return 'nxmeta'
+        if b'<SceneConfig' in data:
+            return 'scnex'
+        if b'<LocalPoints' in data:
+            return 'localweather'
+        if b'GeoBatchHint="0"' in data:
+            return 'gimext'
+        if b'"AssetType":"HapticsData"' in data:
+            return 'haptic'
+        if b'<LocalFogParams' in data:
+            return 'localfogparams'
+        if b'<Audios' in data or b'<AudioSource':
+            return 'prefabaudio'
+        if b'"ReferenceSkeleton' in data:
+            return 'featureschema'
+        if b'<Relationships' in data:
+            return 'xml.rels'
+        if b'<Waterfall' in data:
+            return 'waterfall'
+        if b'"ReferenceSkeletonPath"' in data:
+            return 'mirrortable'
+        if b'<ClothAsset' in data:
+            return 'clt'
+        if b'<plist' in data:
+            return 'plist'
+        if b'<ShaderCompositor' in data or "<ShaderFeature" in data or "<ShaderIndexes" in data or "<RenderTrigger" in data:
+            return 'render'
+        if b'<SkeletonRig' in data:
+            return 'skeletonrig'
+        if b'format: ' in data and b'filter: ' in data:
+            return 'atlas'
+        if b'<ShaderCache' in data:
+            return 'cache'
+        if b'char' in data and 'width=' in data and 'height=' in data:
+            return 'fnt'
+        if b'<AllCaches' in data:
+            return 'info'
+        if b'<AllPreloadCaches' in data:
+            return 'list'
+        if b'<Remove_Files' in data:
+            return 'map'
+        if b'<HLSL File="' in data:
+            return 'md5'
+        if b'<EnvParticle' in data:
+            return 'envp'
+        if b'<FxGroup' in data:
+            return 'sfx'
+        if b'#?RADIANCE' in data:
+            return 'hdr'
+        if b'<Macros' in data:
+            return 'xml.template'
         if b'precision mediump' in data:
             return 'ps'
         if b'POSITION' in data:
@@ -130,8 +204,6 @@ def get_ext(data):
             return 'shader'
         if b'technique' in data or b'ifndef' in data:
             return 'shader'
-        if b'?xml' in data:
-            return 'xml'
         if b'<script' in data:
             return 'html'
         if b'Javascript' in data:
@@ -140,4 +212,6 @@ def get_ext(data):
             return 'bip'
         if b'div.document' in data:
             return 'css'
+    elif data[:1] == b'{':
+        return 'json'
     return 'dat'
