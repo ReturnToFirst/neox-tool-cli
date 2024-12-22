82579b5f5932770e926f295cc303ba3aa32b4212
ReturnToFirst <phj8189@naver.com>
2024-12-23 03:40:58 +0900

    refactor: optimize decompress functions

--- decompress.py
+++ decompress.py
@@ -4,36 +4,44 @@ import zstandard
 import lz4.block
 import os
 
-def init_rotor():
-    asdf_dn = 'j2h56ogodh3se'
-    asdf_dt = '=dziaq.'
-    asdf_df = '|os=5v7!"-234'
-    asdf_tm = asdf_dn * 4 + (asdf_dt + asdf_dn + asdf_df) * 5 + '!' + '#' + asdf_dt * 7 + asdf_df * 2 + '*' + '&' + "'"
-    import rotor
-    rot = rotor.newrotor(asdf_tm)
-    return rot
+from rotor import Rotor
 
-def _reverse_string(s):
-    l = list(s)
-    l = list(map(lambda x: x ^ 154, l[0:128])) + l[128:]
-    l.reverse()
-    return bytes(l)
+# Precompute the rotor key and instantiate the rotor once
+ROT_A = 'j2h56ogodh3se'
+ROT_B = '=dziaq.'
+ROT_C = '|os=5v7!"-234'
+ROT_KEY = ROT_A * 4 + (ROT_B + ROT_A + ROT_C) * 5 + '!' + '#' + ROT_B * 7 + ROT_C * 2 + '*' + '&' + "'"
 
-def nxs_unpack(data):
+ROT = Rotor(ROT_KEY)
+
+# Load libraries once to reuse later
+if os.name == "posix":
+    lib = ctypes.CDLL("./libs/libpubdecrypt.so")
+elif os.name == "nt":
+    lib = ctypes.CDLL("./libs/libpubdecrypt.dll")
+else:
+    lib = None  # In case the platform is unsupported
+
+def _reverse_bytes(byte_arr: bytearray) -> bytearray:
+    
+    for i in range(128):
+        byte_arr[i] ^= 154
+    
+    byte_arr.reverse()
+
+    return bytes(byte_arr)
+
+def nxs_unpack(data: bytearray):
+    if not lib:
+        raise RuntimeError("No supported library found")
+
     wrapped_key = ctypes.create_string_buffer(4)
     data_in = ctypes.create_string_buffer(data[20:])
-
-    if os.name == "posix":
-        liblinux = ctypes.CDLL("./libs/libpubdecrypt.so")
-        returning = liblinux.public_decrypt(data_in, wrapped_key)
-    elif os.name == "nt":
-        libwindows = ctypes.CDLL("./libs/libpubdecrypt.dll")
-        returning = libwindows.public_decrypt(data_in, wrapped_key)
-
+    
+    lib.public_decrypt(data_in, wrapped_key)
 
     ephemeral_key = int.from_bytes(wrapped_key.raw, "little")
-
-    decrypted = []
+    decrypted = bytearray()
 
     for i, x in enumerate(data[20 + 128:]):
         val = x ^ ((ephemeral_key >> (i % 4 * 8)) & 0xff)
@@ -42,30 +50,23 @@ def nxs_unpack(data):
             ephemeral_key = (ror + ((ror << 2) & 0xFFFFFFFF) + 0xE6546B64) & 0xFFFFFFFF
         decrypted.append(val)
 
-    decrypted = bytes(decrypted)
-    return decrypted
+    return bytes(decrypted)
 
-def zflag_decompress(flag, data, origlength=0):
-    match flag:
-        case 1:
-            return zlib.decompress(data, bufsize=origlength)
-        case 2:
-            return lz4.block.decompress(data,uncompressed_size=0x0FFFFFFE)
-        case 3:
-            return zstandard.ZstdDecompressor().decompress(data)
-        case 5:
-            return data
+def zflag_decompress(flag: int, data: bytearray, original_size: int=0):
+    if flag == 1:
+        return zlib.decompress(data, bufsize=original_size)
+    elif flag == 2:
+        return lz4.block.decompress(data, uncompressed_size=0x0FFFFFFE)
+    elif flag == 3:
+        return zstandard.ZstdDecompressor().decompress(data)
+    elif flag == 5:
+        return data
     return data
 
-def special_decompress(flag, data):
-    match flag:
-        case "rot":
-            rotor = init_rotor()
-            return _reverse_string(zlib.decompress(rotor.decrypt(data)))
-            
-        case "nxs3":
-            buf = nxs_unpack(data)
-            return lz4.block.decompress(buf, int.from_bytes(data[16:20], "little"))
+def special_decompress(flag: int, data: bytearray) -> bytearray:
+    if flag == "rot":
+        return  _reverse_bytes(zlib.decompress(ROT.decrypt(data)))
+    elif flag == "nxs3":
+        buf = nxs_unpack(data)
+        return lz4.block.decompress(buf, int.from_bytes(data[16:20], "little"))
     return data
-            
-    
